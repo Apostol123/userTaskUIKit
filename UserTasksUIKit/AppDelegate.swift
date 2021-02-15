9e6c425e1eba7508816ae4e2be50e3a4f1d9a521
Alexandru <aual@gft.com>
2021-02-15 10:22:22 +0100

    add gitignore

--- UserTasksUIKit/AppDelegate.swift
+++ UserTasksUIKit/AppDelegate.swift
@@ -11,9 +11,14 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window  = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: ViewController())
+         window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+       
         // Override point for customization after application launch.
         return true
     }
--- UserTasksUIKit/ViewController.swift
+++ UserTasksUIKit/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        print("SIIIU")
+    }
 
 }
 
