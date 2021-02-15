ec0866b86d1a82ed4be5dc4339b556724ce71d67
Alexandru <aual@gft.com>
2021-02-15 09:33:07 +0100

    Initial Commit

---
+++ UserTasksUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserTasksUIKit
+//
+//  Created by Apostol, Alexandru on 15/02/2021.
+//  Copyright © 2021 Apostol, Alexandru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
