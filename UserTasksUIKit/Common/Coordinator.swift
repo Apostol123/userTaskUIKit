e2b632e93f7c9442e22c3089f841bf1b6f314745
Alexandru <aual@gft.com>
2021-02-15 10:41:04 +0100

    create AppCoordinator implement AppCoordinator skeleton

---
+++ UserTasksUIKit/Common/Coordinator.swift
@@ -0,0 +1,9 @@
+//
+//  Coordinator.swift
+//  UserTasksUIKit
+//
+//  Created by Apostol, Alexandru on 15/02/2021.
+//  Copyright © 2021 Apostol, Alexandru. All rights reserved.
+//
+
+import Foundation
